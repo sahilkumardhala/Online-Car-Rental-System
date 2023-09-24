a0f9c0c5acd73faae285cd1081078f3edb1d4d28
[sahilkumardhala] <sahildhala123@gmail.com>
2023-09-24 10:24:01 +0530

    1st commit

---
+++ admin/vendor/inc/footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; <?php echo date('Y');?> Online Car Rentals System | Crafted With  <i class ="fa fa-heartbeat"></i> Bharadwaj Nayak
+           </span>
+          </div>
+        </div>
+</footer>
---
+++ vehiclebooking/usr/vendor/inc/config.php
@@ -0,0 +1,7 @@
+<?php
+$dbuser="root";
+$dbpass="";
+$host="localhost";
+$db="vehicle_bookings";
+$mysqli=new mysqli($host,$dbuser, $dbpass, $db);
+?>
